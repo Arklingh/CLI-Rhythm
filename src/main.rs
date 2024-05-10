1f179464587f16be70ee994fa494c02b5e3eaef5
Arklingh <andriikurochka@ukr.net>
2024-05-10 22:28:20 +0300

    Made scanning func for cur and Music dirs

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
 extern crate crossterm;
 extern crate tui;
 
-use std::io;
+use std::env;
 use std::io::stdout;
 use std::path::PathBuf;
-use std::sync::{Arc, Mutex};
+use std::{fs, io};
 
 use crossterm::event::{Event, KeyCode, KeyEvent, KeyEventKind, KeyEventState, KeyModifiers};
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear};
 use crossterm::ExecutableCommand;
-use rodio::{OutputStream, Sink};
 use tui::backend::CrosstermBackend;
 use tui::layout::{Constraint, Direction, Layout};
 use tui::style::{Color, Modifier, Style};
 use tui::text::Text;
 use tui::widgets::{Block, Borders, Gauge, List, ListItem, Paragraph};
 use tui::Terminal;
+
+use dirs;
+
+use audiotags::{types::Album, Tag};
+use mp3_metadata::read_from_file;
 
 const MUSIC_FORMATS: [&str; 10] = [
     "mp3", "wav", "flac", "aac", "ogg", "wma", "m4a", "alac", "ape", "opus",
@@ -54,9 +58,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     stdout().execute(Clear(crossterm::terminal::ClearType::All))?;
 
-    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    let sink = Arc::new(Mutex::new(Sink::try_new(&stream_handle).unwrap()));
-
     // Run event loop
     loop {
         terminal.draw(|f| {
@@ -86,7 +87,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .enumerate()
                 .map(|(_index, song)| {
                     let style = Style::default();
-
                     ListItem::new(song.to_string().clone()).style(style)
                 })
                 .collect();
@@ -129,8 +129,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             let volume_bar = Gauge::default()
                 .block(Block::default().borders(Borders::ALL).title("Volume"))
                 .gauge_style(Style::default().fg(Color::LightBlue))
-                .label(format!("100%"))
-                .ratio(sink.lock().unwrap().volume() as f64);
+                .label(format!("100%"));
+
             f.render_widget(volume_bar, footer[1]);
         })?;
 
@@ -155,4 +155,112 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Cleanup
     disable_raw_mode()?;
     Ok(())
+}
+
+fn scan_folder_for_music() -> Vec<Song> {
+    let current_folder = match dirs::audio_dir() {
+        Some(dir) => dir,
+        None => env::current_dir().unwrap(),
+    };
+
+    let song_paths = match fs::read_dir(&current_folder) {
+        Ok(entries) => {
+            let music_files: Vec<PathBuf> = entries
+                .filter_map(|entry| {
+                    if let Ok(entry) = entry {
+                        let path = entry.path();
+                        if path.is_file() {
+                            if let Some(ext) = path.extension() {
+                                if let Some(ext_str) = ext.to_str() {
+                                    if MUSIC_FORMATS.contains(&ext_str) {
+                                        Some(path)
+                                    } else {
+                                        None
+                                    }
+                                } else {
+                                    None
+                                }
+                            } else {
+                                None
+                            }
+                        } else {
+                            None
+                        }
+                    } else {
+                        None
+                    }
+                })
+                .collect();
+            music_files
+        }
+        Err(e) => {
+            eprintln!("Error reading directory: {}", e);
+            panic!("awawawa");
+        }
+    };
+
+    let mut song_list: Vec<Song> = Vec::new();
+    for song in song_paths {
+        let current_song;
+        if song.ends_with("mp3") {
+            let mp3_meta = read_from_file(&song).unwrap();
+            let mp3_clone = read_from_file(&song).unwrap();
+            let mp3_a = read_from_file(&song).unwrap();
+
+            current_song = Song::new(
+                mp3_meta.tag.unwrap().title,
+                mp3_a.tag.unwrap().artist,
+                song.clone(),
+                mp3_clone.tag.unwrap().album,
+                mp3_meta.duration.as_secs_f64(),
+            );
+        } else {
+            let mut mp3_duration: f64 = 0.0;
+            if song.extension().unwrap().to_str().unwrap() == "mp3" {
+                mp3_duration = read_from_file(&song).unwrap().duration.as_secs_f64();
+            }
+            let meta = Tag::new().read_from_path(&song).unwrap();
+
+            current_song = Song::new(
+                meta.title().unwrap_or("No Title").to_string(),
+                meta.artist().unwrap_or("No Title").to_string(),
+                song.clone(),
+                meta.album()
+                    .unwrap_or(Album {
+                        title: "None",
+                        artist: None,
+                        cover: None,
+                    })
+                    .title
+                    .to_string(),
+                if let Some(ext) = song.extension().and_then(|e| e.to_str()) {
+                    match ext {
+                        "mp3" => mp3_duration,
+                        _ => meta.duration().unwrap_or(0.0_f64),
+                    }
+                } else {
+                    meta.duration().unwrap_or(0.0_f64)
+                },
+            );
+        }
+        song_list.push(current_song);
+    }
+
+    if song_list.is_empty() {
+        song_list.push(Song::new(
+            "No songs in current directory!".to_string(),
+            "No Title".to_string(),
+            PathBuf::new(),
+            Album {
+                title: "None",
+                artist: None,
+                cover: None,
+            }
+            .title
+            .to_string(),
+            0.0_f64,
+        ));
+    }
+
+    song_list
 }
